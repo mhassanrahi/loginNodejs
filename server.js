e156506819603de51f51b3423dd6454c526f7669
Hassan <m.hassan.rahi@gmail.com>
2019-12-30 14:33:32 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,99 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const express = require('express');
+const app = express();
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id),
+)
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+let users = [];
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+
+app.post('/register', checkNotAuthenticated, async(req, res) => {
+
+    try {
+        const sName = req.body.name;
+        const sEmail = req.body.email;
+        const sPassword = req.body.password;
+        const hashedPassword = await bcrypt.hash(sPassword, 10)
+
+        users.push({
+            id: Date.now().toString(),
+            name: sName,
+            email: sEmail,
+            password: hashedPassword
+        })
+
+        res.redirect('/login')
+
+    } catch (error) {
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('UP and running on port ' + port)
+})
